c082ae68c5811c4b6e4699486677dea8d91912bf
Calytic <calytic@umod.org>
2019-10-19 02:54:14 -0400

    Very advanced plugin

---
+++ TestPlugin.cs
@@ -0,0 +1,18 @@
+using System;
+using Oxide.Core;
+
+namespace Oxide.Plugins
+{
+    [Info ("Test", "Calytic", "0.0.42")]
+    [Description ("Test")]
+    class TestPlugin : CovalencePlugin
+    {
+        [Command ("Test")]
+        void cmdTest ()
+        {
+            webrequest.Enqueue ("https://httpbin.com/get", string.Empty, delegate (int status, string response) {
+                Interface.Oxide.LogInfo ("Response1\n" + response);
+            }, this);
+        }
+    }
+}
